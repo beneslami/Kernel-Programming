2125875368b6d0e9374a08e03fe9ec42bcb59819
Benyamin <beneslami72@gmail.com>
2022-06-13 17:12:56 +0200

    update

--- MyModule.c
+++ MyModule.c
@@ -11,19 +11,36 @@ static struct sock *nl_sk = NULL;
 
 static void netlink_recv_msg_fn(struct sk_buff *skb_in){
     struct nlmsdgdr *nlh_recv, *nlk_reply;
+    int user_space_port_id;
+    char *user_space_data;
+    int user_space_data_len;
     char kernel_reply[256];
     struct sk_buff *skb_out;
     printk(KERN_INFO "%s invoked", __FUNCTION__ );
     nlk_recv = (struct nlmsghdr*)(skb_in->data);
-    nlmsg_dump(nlh_recv);
+    //nlmsg_dump(nlh_recv);
     user_space_process_port_id = nlh_recv->nlmsg_pid;
-    printk(KERN_INFO "%s(%d): port id of the sending user space process =  %u\n", __FUNCTION__ , __LINE__, user_space_process_port_id);
+    printk(KERN_INFO "%s(%d): port id of the sending user space process = %u\n", __FUNCTION__ , __LINE__, user_space_process_port_id);
     user_space_data = (char*)nlmsg_data(nlh_recv);
     user_space_data_len = skb_in->len;
+    printk(KERN_INFO "%s(%d) : message received from userspace = %s ", __FUNCTION__);
     if(nlh_recv->nlmsg_flags & NLM_F_ACK){
         memset(kernel_reply, 0, sizeof(kernel_reply));
-        snprintf(kernel_reply, sizeof kernel_reply), "MSG from process %d has been processed by kernel", nlh_recv->nlmsg_pid);
+        snprintf(kernel_reply, sizeof kernel_reply, "MSG from process %d has been processed by kernel", nlh_recv->nlmsg_pid);
         skb_out = nlmsg_new(sizeof(kernel_reply), 0);
+        nlh_reply = nlmsg_put(skb_out,
+                              0,
+                              nlh_recv->nlmsg_seq,
+                              NLMSG_DONE,
+                              sizeof(kernel_reply),
+                              0
+                              );
+        strncpy(nlmsg_data(nlh_reply), kernel_reply, sizeof(kernel_reply));
+        res = nlmsg_unicast(nl_sk, skb_out, user_space_port_id);
+        if(res < 0){
+            printk(KERN_INFO "Error while sendingg the data back to userspace\n");
+            kfree_skb(skb_out);
+        }
     }
 }
 
@@ -38,6 +55,7 @@ static int __init NetlinkGreetings_init(void){
         printk(KERN_INFO "kernel netlink socket for netlink protocol %u failed\n", NETLINK_TEST_PROTOCOL);
         return _ENOMEM;
     }
+    printk(KERN_INFO, "Netlink socket created successfully");
     return 0;
 }
 
@@ -50,4 +68,6 @@ static void __exit NetlinkGreetings_exit(void){
 moduel_init(NetlinkGreetings_init);
 module_exit(NetlinkGreetings_exit);
 
+MODULE_AUTHOR("BEN ESLAMI");
+MODULE_DESCRIPTION("Netlink Example");
 MODULE_LICENSE("GPL");
